804470a70ce183684436cc411959e3256f949fbf
celsocundiati <cundiaticelso@gmail.com>
2026-01-21 15:03:37 +0100

    home sucess

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import Home from './componentes/home/home';
 
 function App() {
   return (
+    // Dev: Celso
     <main className="max-w-7xl mx-auto px-6 py-8">
       
       <Header navLinks={navLinks} />
       
       <Home />
-
-
+    
     </main>
   );
 }
