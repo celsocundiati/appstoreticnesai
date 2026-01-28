040435974adf0c976e17f16272e6643a4b9bfe8f
celsocundiati <cundiaticelso@gmail.com>
2026-01-28 14:19:42 +0100

    Sucess02

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './componentes/home/home';
 import Publicar from './componentes/publicarapp/publicar';
 import Perfil from './componentes/perfil/perfil';
-import AppDetalhes from './componentes/detalhes/detalhes';
+import AppDetalhes from './componentes/detalhes/Detalhes';
 
 function App() {
   return (
